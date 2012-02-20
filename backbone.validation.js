c73d502d4f120f9640286718a2217cfc04dc9852
thedersen <thedersen@gmail.com>
2012-02-20 23:33:55 +0100

    Can pass the name of an attribute or an array of names to  to verify if the attribute(s) are valid

--- backbone.validation.js
+++ backbone.validation.js
@@ -125,8 +125,19 @@ Backbone.Validation = (function(Backbone, _, undefined) {
 
     var mixin = function(view, options) {
         return {
-            isValid: function(forceValidation) {
-                if(forceValidation) {
+            isValid: function(option) {
+                if(_.isString(option)){
+                    return !validateAttr(this, this.validation, option, this.get(option), this.toJSON());
+                }
+                if(_.isArray(option)){
+                    for (var i = 0; i < option.length; i++) {
+                        if(validateAttr(this, this.validation, option[i], this.get(option[i]), this.toJSON())){
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+                if(option === true) {
                     this.validate();
                 }
                 return this.validation ? this._isValid : true;
