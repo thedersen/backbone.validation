2b31ded71211eadb60bc433bc57c3bb84e8f1774
Vladimir Tsvang <vtsvang@gmail.com>
2012-12-14 17:21:17 +0400

    Fixed Sizzle error: "unrecognized expression"

--- src/backbone-validation.js
+++ src/backbone-validation.js
@@ -343,7 +343,7 @@ Backbone.Validation = (function(_){
     // view becomes valid. Removes any error message.
     // Should be overridden with custom functionality.
     valid: function(view, attr, selector) {
-      view.$('[' + selector + '~=' + attr + ']')
+      view.$('[' + selector + '~="' + attr + '"]')
           .removeClass('invalid')
           .removeAttr('data-error');
     },
@@ -352,7 +352,7 @@ Backbone.Validation = (function(_){
     // Adds a error message.
     // Should be overridden with custom functionality.
     invalid: function(view, attr, error, selector) {
-      view.$('[' + selector + '~=' + attr + ']')
+      view.$('[' + selector + '~="' + attr + '"]')
           .addClass('invalid')
           .attr('data-error', error);
     }
