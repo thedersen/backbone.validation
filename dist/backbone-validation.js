a4e934c348af5ec67f00d49a42f255660e59b1e5
nuragic <me@nuragic.io>
2015-01-21 10:31:24 +0100

    Bumped version to 0.9.2

--- dist/backbone-validation.js
+++ dist/backbone-validation.js
@@ -1,6 +1,6 @@
-// Backbone.Validation v0.9.1
+// Backbone.Validation v0.9.2
 //
-// Copyright (c) 2011-2014 Thomas Pedersen
+// Copyright (c) 2011-2015 Thomas Pedersen
 // Distributed under MIT License
 //
 // Documentation and full license available at:
@@ -64,13 +64,7 @@ Backbone.Validation = (function(_){
 
     _.each(obj, function(val, key) {
       if(obj.hasOwnProperty(key)) {
-        if (val && typeof val === 'object' && !(
-          val instanceof Array ||
-          val instanceof Date ||
-          val instanceof RegExp ||
-          val instanceof Backbone.Model ||
-          val instanceof Backbone.Collection)
-        ) {
+        if (!!val && typeof val === 'object' && val.constructor === Object) {
           flatten(val, into, prefix + key + '.');
         }
         else {
