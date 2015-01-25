afd68f2444d71ae9a22c0efd8f0ca6714bcd468c
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2015-01-24 23:06:21 -0600

    Added failing unit tests for intermediate-level nested validation.

--- tests/nestedValidation.js
+++ tests/nestedValidation.js
@@ -271,6 +271,129 @@ buster.testCase('Nested validation', {
     }
   },
 
+  "complex nesting with intermediate-level validators": {
+    setUp: function() {
+      this.valid = this.spy();
+      this.invalid = this.spy();
+  
+      var Model = Backbone.Model.extend({
+        validation: {
+          'foo.bar': {
+            fn: 'validateBazAndQux',
+            msg: 'bazQuxError1'
+          },
+          'foo.foo': {
+            fn: 'validateBazAndQux',
+            msg: 'bazQuxError2'
+          }
+        },
+        validateBazAndQux: function (value, attr, computedState) {
+          if (!value || !value.baz || !value.qux) {
+            return "error";
+          }
+        }
+      });
+  
+      this.model = new Model();
+      this.view = new Backbone.View({model: this.model});
+      
+      Backbone.Validation.bind(this.view, {
+        invalid: this.invalid,
+        valid: this.valid
+      });
+    },
+
+    "invalid": {
+      setUp: function () {
+        this.result = this.model.set({
+          foo: {
+            bar: {
+              baz: '',
+              qux: 'qux'
+            },
+            foo: {
+              baz: 'baz',
+              qux: ''
+            }
+          }
+        }, {validate: true});
+      },
+  
+      "refutes setting invalid values": function() {
+        refute(this.result);
+      },
+  
+      "calls the invalid callback": function() {
+        assert.calledWith(this.invalid, this.view, 'foo.bar', 'bazQuxError1');
+        assert.calledWith(this.invalid, this.view, 'foo.foo', 'bazQuxError2');
+      },
+  
+      "isValid returns false for the specified attribute name": function () {
+        refute(this.model.isValid('foo.bar'));
+        refute(this.model.isValid('foo.foo'));
+      },
+  
+      "isValid returns false for the specified attribute names": function () {
+        refute(this.model.isValid(['foo.foo', 'foo.bar']));
+      },
+  
+      "preValidate returns error message for the specified attribute name": function () {
+        assert(this.model.preValidate('foo.bar', ''));
+        assert(this.model.preValidate('foo.foo', ''));
+      },
+
+      "preValidate does not return error message if new nested values validate": function () {
+        refute(this.model.preValidate('foo.bar', { baz: 1, qux: 1 }));
+        refute(this.model.preValidate('foo.foo', { baz: 1, qux: 1 }));
+      }
+    },
+
+    "valid": {
+      setUp: function () {
+        this.result = this.model.set({
+          foo: {
+            bar: {
+              baz: 'val',
+              qux: 'val'
+            },
+            foo: {
+              baz: 'val',
+              qux: 'val'
+            }
+          }
+        }, {validate: true});
+      },
+  
+      "sets the value": function() {
+        assert(this.result);
+      },
+  
+      "calls the valid callback": function() {
+        assert.calledWith(this.valid, this.view, 'foo.bar');
+        assert.calledWith(this.valid, this.view, 'foo.foo');
+      },
+  
+      "isValid returns true for the specified attribute name": function () {
+        assert(this.model.isValid('foo.bar'));
+        assert(this.model.isValid('foo.foo'));
+      },
+  
+      "isValid returns true for the specified attribute names": function () {
+        assert(this.model.isValid(['foo.bar', 'foo.foo']));
+      },
+  
+      "preValidate returns no error message for the specified attribute name": function () {
+        refute(this.model.preValidate('foo.bar', { baz: 1, qux: 1 }));
+        refute(this.model.preValidate('foo.foo', { baz: 1, qux: 1 }));
+      },
+
+      "preValidate returns error message if new nested values do not validate": function () {
+        assert.equals(this.model.preValidate('foo.bar', { baz: '', qux: '' }), 'bazQuxError1');
+        assert.equals(this.model.preValidate('foo.foo', { baz: '', qux: '' }), 'bazQuxError2');
+      }
+    }
+  },
+
   "nested models and collections": {
     setUp: function () {
       this.valid = this.spy();
